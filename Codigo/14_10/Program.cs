1b937b2aadbb8f88b8b886eab603cb776e85987f
BrunoBafilli <brunobafilli@gmail.com>
2023-10-14 12:02:10 -0300

    Variaveis e Console
    Capturando dados digitados.

---
+++ Codigo/14_10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Digite um numero");
+int n1 = Convert.ToInt32(Console.ReadLine());
+int n2 = Convert.ToInt32(Console.ReadLine());
+int resultado = n1 + n2;
+Console.WriteLine(resultado);
