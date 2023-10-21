193ce040b619ba574f9446914bcdb389ed81ea0a
BrunoBafilli <brunobafilli@gmail.com>
2023-10-21 12:16:35 -0300

    logn
    logn

---
+++ Códigos/21_10/Program.cs
@@ -0,0 +1,9 @@
+﻿//Convert.ToInt32(Console.ReadLine());// captura um valor digitado inteiro.
+//Console.WriteLine("ola"); // mostra mensagem na tela.
+int idade = 20;
+bool maiordeidade = idade > 22;
+if(maiordeidade){
+    Console.WriteLine("entrou no if");
+} else {
+    Console.WriteLine("entrou no else");
+}
