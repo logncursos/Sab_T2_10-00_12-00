5eadcfb67b1a7431e68cc1e87c38f84f3de17091
BrunoBafilli <brunobafilli@gmail.com>
2023-11-04 12:16:50 -0300

    t
    t

--- Códigos/04_11_23/Program.cs
+++ Códigos/04_11_23/Program.cs
@@ -1,35 +1,7 @@
-﻿// Desafio: Calculadora Simples
-// 
-// Crie um programa em C# que funcione como uma calculadora simples. O programa deve pedir ao usuário que insira dois números e uma operação (adição, subtração, multiplicação ou divisão). Em seguida, ele deve calcular o resultado com base na operação inserida e exibir o resultado.
-// ------------------------------------------
-// Regras:
-// Solicite ao usuário que insira o primeiro número, a operação (utilize "+", "-", "*", ou "/") e o segundo número.
-// Use uma estrutura if e else para determinar qual operação o usuário deseja realizar.
-// Realize a operação correspondente e exiba o resultado.
-// ------------------------------------------
-// Exemplo de saída:
-// Digite o primeiro número: 10
-// Digite a operação (+, -, *, /): *
-// Digite o segundo número: 5
-// Resultado: 50
-// ------------------------------------------
-// Dica:
-// Você pode usar as operações matemáticas +, -, * e / para calcular o resultado com base na operação inserida pelo usuário. Certifique-se de tratar casos em que a operação pode ser inválida (por exemplo, divisão por zero).
+﻿// Desafio: Contagem de números pares e ímpares
+//
+// Crie um programa que conte quantos números
+// pares e ímpares existem em um intervalo de
+// números fornecido pelo usuário (por exemplo, de 1 a 100).
+// Em seguida, exiba o total de números pares e ímpares encontrados.
 
-Console.WriteLine("Digite um numero");
-int numero1 = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Digite um opeardor -, +, *, /, ");
-string operador = Console.ReadLine();
-
-Console.WriteLine("Digite outro numero");
-int numero2 = Convert.ToInt32(Console.ReadLine());
-
-if(operador == "*"){
-    int resultado = numero1 * numero2;
-    Console.WriteLine(resultado);
-} 
-if(operador == "/"){
-    int resultado = numero1 / numero2;
-    Console.WriteLine(resultado);
-}
