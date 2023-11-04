a62aa4a38eba11d525f663e77f4588a43dbbdee1
BrunoBafilli <brunobafilli@gmail.com>
2023-11-04 10:23:09 -0300

    aula
    aula

---
+++ Códigos/04_11_23/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] original = new int[] {6,4};
+
+int[] vendedor1 = new int[2];
+
+int[] vendedor2 = new int[2];
+
+for (int i = 0; i < original.Length; i++)
+{
+    vendedor1[i] = original[i];
+    vendedor2[i] = original[i];
+}
+
+Console.WriteLine("vendedor1");
+for (int i = 0; i < vendedor1.Length; i++)
+{
+    Console.Write(vendedor1[i] -1 +",");
+}
+
+Console.WriteLine("vendedor2");
+for (int i = 0; i < vendedor1.Length; i++)
+{
+    Console.Write(vendedor2[i]-2+",");
+}
